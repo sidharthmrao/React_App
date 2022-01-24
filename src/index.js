e3ea1027107955d5d7af8f70701dd29c2c6cfdce
Sidharth Rao <sidharthmrao@gmail.com>
2022-01-23 23:48:39 -0500

    Created Counter Component and added incrementation

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import 'bootstrap/dist/css/bootstrap.css';
+import Counter from './components/counterComponent';
+
 
 const element = <h1>Hello World</h1>;
 //console.log(element);
-ReactDOM.render(element, document.getElementById('bruh')); //rendering the element to the DOM - Should be 'root' lol but i made it 'bruh'
+ReactDOM.render(<Counter/>, document.getElementById('bruh')); //rendering the element to the DOM - Should be 'root' lol but i made it 'bruh'
